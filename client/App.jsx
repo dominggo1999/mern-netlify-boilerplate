840f6aae0df2487e4219cb68d38ccbde320b9751
dominggo1999 <arnolddominggo19@gmail.com>
2022-08-06 14:38:56 +0700

    init

---
+++ client/App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import tw, { styled } from 'twin.macro';
+import Hello from '~/components/Hello';
+import { backendURL } from '~/config/backendUrl';
+
+export const Box = styled.div`
+  ${tw`
+    w-full 
+    h-screen 
+    bg-black 
+    flex  
+    flex-col
+    justify-center
+    items-center
+    text-white
+    text-2xl
+  `}
+`;
+
+const App = () => {
+  const [message, setMessage] = useState('');
+
+  const callBackend = async () => {
+    const res = await fetch(backendURL);
+    const json = await res.json();
+    setMessage(json.message);
+  };
+
+  useEffect(() => {
+    callBackend();
+  }, []);
+
+  return (
+    <Box>
+      <Hello />
+      <p>{message}</p>
+    </Box>
+  );
+};
+
+export default App;
